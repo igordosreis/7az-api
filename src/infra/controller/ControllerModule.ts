dbfeaf778b375868a2302097eb6060ea4b3e1f0f
igordosreis <igorreis@gmail.com>
2024-07-12 12:21:40 -0300

    fix: controller modules

--- src/infra/controller/ControllerModule.ts
+++ src/infra/controller/ControllerModule.ts
@@ -1,25 +1,21 @@
 import { Module } from '@nestjs/common';
 import { RouterModule, Routes } from '@nestjs/core';
 import ZodPipeLine from '@infra/middleware/global/zod/ZodPipeLine';
-import ValidateAdminModule from '@infra/integration/validateAdmin/ValidateAdminModule';
-import InvoiceUseCaseProxyModule from '@infra/usecaseproxy/invoice/InvoiceUseCaseProxyModule';
-import InvoiceController from './invoice/InvoiceController';
+import InvoiceControllerModule from './invoice/InvoiceControllerModule';
 
 const routes: Routes = [
   {
-    path: ' 7az/invoice',
-    module: InvoiceController,
+    path: '7az/invoice',
+    module: InvoiceControllerModule,
   },
 ];
 
 @Module({
   imports: [
-    ValidateAdminModule,
-    InvoiceUseCaseProxyModule,
+    InvoiceControllerModule,
     RouterModule.register(routes),
   ],
   providers: [ZodPipeLine],
-  controllers: [InvoiceController],
 })
 class ControllerModule {}
 
